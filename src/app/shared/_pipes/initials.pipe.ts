d788280d8b285978eee1e2f448d19e0cf9bfb67e
Claudio Manuel Andrade da Silva <claudio351445@gmail.com>
2024-09-05 12:52:38 -0300

    Inclusão do README

--- src/app/shared/_pipes/initials.pipe.ts
+++ src/app/shared/_pipes/initials.pipe.ts
@@ -5,7 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true
 })
 export class InitialsPipe implements PipeTransform {
-
+ /**
+  * Pipe utilzado para pegar um valor e utilizar suas as iniciais para criar valor para ser exibidos nos cards
+  * @param value string utilizada para fazer a conversão em iniciais
+  * @returns 
+  */
   transform(value: any): string {
     const word = value.split(' ');
     const initial = word.length > 1 ? `${word[0].charAt(0)}${word[1].charAt(0)}` : `${word[0].charAt(0)}${word[0].charAt(1)}`
